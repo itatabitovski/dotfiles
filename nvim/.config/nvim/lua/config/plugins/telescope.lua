8d37d2ddfc58cf76ea1227912eb339287b2f7bd7
Ilin Tatabitovski <itatabitovski@gmail.com>
2026-06-13 20:36:58 +0200

    feat(neovim): telescope find_files show hidden files

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -10,6 +10,12 @@ return {
     telescope.setup({
       defaults = {
         path_display = { "smart" },
+        file_ignore_patterns = { "node_modules", ".git/" },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
       },
     })
 
